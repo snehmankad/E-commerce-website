f5134a8e8d10063d1e6ccd89a1de3a0bee9387ca
snehmankad <sneh.mankad@gmail.com>
2020-10-04 02:12:14 +0530

    added gitignore and procfile

--- my_ecommerce_app/settings.py
+++ my_ecommerce_app/settings.py
@@ -134,7 +134,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/' # for CSS files
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL =    '/'
 
